b282f03272073c8cfef11ffc44c58c3b1b577539
GedMarc <ged_marc@hotmail.com>
2019-05-06 01:57:10 +0200

    General Updates DataTables and BS4 Split for security modules WebSocket updates

--- src/main/java/com/jwebmp/plugins/security/localstorage/implementations/LocalStorageKeyWSAuth.java
+++ src/main/java/com/jwebmp/plugins/security/localstorage/implementations/LocalStorageKeyWSAuth.java
@@ -3,19 +3,24 @@ package com.jwebmp.plugins.security.localstorage.implementations;
 import com.jwebmp.websockets.WebSocketsConfiguration;
 import com.jwebmp.websockets.services.IWebSocketAuthDataProvider;
 
+import static com.jwebmp.core.utilities.StaticStrings.*;
+
 public class LocalStorageKeyWSAuth
 		implements IWebSocketAuthDataProvider<LocalStorageKeyWSAuth>
 {
 	@Override
 	public StringBuilder getJavascriptToPopulate()
 	{
-		return new StringBuilder("jw.localstorage['jwamsmk']");
+		return new StringBuilder("jw.websocket.newMessage('LocalStorage'," +
+		                         "{'jwamsmk':jw.localstorage['jwamsmk']}" +
+		                         ");"
+		);
 	}
 
 	@Override
 	public String name()
 	{
-		return "jwamsmk";
+		return "LocalStorageWS";
 	}
 
 	@Override
