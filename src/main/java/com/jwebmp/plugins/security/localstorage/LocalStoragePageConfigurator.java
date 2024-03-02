3029bf3e0af65f94ada40cf3613b2492209580d7
GedMarc <marc.magon@gmail.com>
2024-03-02 20:36:41 +0200

    Add Browser Storage

--- src/main/java/com/jwebmp/plugins/security/localstorage/LocalStoragePageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/security/localstorage/LocalStoragePageConfigurator.java
@@ -9,67 +9,67 @@ import jakarta.validation.constraints.*;
 
 @Singleton
 @PluginInformation(pluginName = "Local Storage ",
-                   pluginUniqueName = "local-storage",
-                   pluginDescription = "Provides access to read and write into the local storage of a browser.",
-                   pluginVersion = "1.0",
-                   pluginDependancyUniqueIDs = "jquery",
-                   pluginCategories = "storage,cookies,localstorage,security",
-                   pluginSubtitle = "Local Storage Service ",
-                   pluginGitUrl = "https://github.com/GedMarc/JWebMP-LocalStorageSecurity",
-                   pluginSourceUrl = "https://github.com/GedMarc/JWebMP-LocalStorageSecurity",
-                   pluginWikiUrl = "https://github.com/GedMarc/JWebMP-LocalStorageSecurity/wiki",
-                   pluginOriginalHomepage = "https://github.com/GedMarc/JWebMP-LocalStorageSecurity",
-                   pluginDownloadUrl = "https://mvnrepository.com/artifact/com.jwebmp.plugins.security/jwebmp-plugins-local-storage-security",
-                   pluginIconUrl = "",
-                   pluginIconImageUrl = "",
-                   pluginLastUpdatedDate = "2022/03/07",
-                   pluginGroupId = "com.jwebmp.plugins.security",
-                   pluginArtifactId = "jwebmp-plugins-local-storage-security",
-                   pluginModuleName = "com.jwebmp.plugins.security.localstorage",
-                   pluginStatus = PluginStatus.Released
+        pluginUniqueName = "local-storage",
+        pluginDescription = "Provides access to read and write into the local storage of a browser.",
+        pluginVersion = "1.0",
+        pluginDependancyUniqueIDs = "jquery",
+        pluginCategories = "storage,cookies,localstorage,security",
+        pluginSubtitle = "Local Storage Service ",
+        pluginGitUrl = "https://github.com/GedMarc/JWebMP-LocalStorageSecurity",
+        pluginSourceUrl = "https://github.com/GedMarc/JWebMP-LocalStorageSecurity",
+        pluginWikiUrl = "https://github.com/GedMarc/JWebMP-LocalStorageSecurity/wiki",
+        pluginOriginalHomepage = "https://github.com/GedMarc/JWebMP-LocalStorageSecurity",
+        pluginDownloadUrl = "https://mvnrepository.com/artifact/com.jwebmp.plugins.security/jwebmp-plugins-local-storage-security",
+        pluginIconUrl = "",
+        pluginIconImageUrl = "",
+        pluginLastUpdatedDate = "2022/03/07",
+        pluginGroupId = "com.jwebmp.plugins",
+        pluginArtifactId = "localstorage",
+        pluginModuleName = "com.jwebmp.plugins.security.localstorage",
+        pluginStatus = PluginStatus.Released
 )
 @NgBootConstructorParameter("private socketClientService : SocketClientService")
 @NgBootConstructorBody(value = "this.socketClientService.send('LocalStorage',{},'localStorage');", sortOrder = 5)
 public class LocalStoragePageConfigurator
-		implements IPageConfigurator<LocalStoragePageConfigurator>
+        implements IPageConfigurator<LocalStoragePageConfigurator>
 {
-	/**
-	 * The local storage
-	 */
-	private boolean localStorage = true;
-	
-	@Override
-	public @NotNull Page<?> configure(Page<?> page)
-	{
-		//	page.getBody()
-		//	    .addJavaScriptReference(PersistJS.getJavaScriptReference());
-		return page;
-	}
-	
-	@Override
-	public boolean enabled()
-	{
-		return localStorage;
-	}
-	
-	
-	/**
-	 * If this page should be rendered with dynamic local storage support
-	 *
-	 * @return
-	 */
-	public boolean isLocalStorage()
-	{
-		return localStorage;
-	}
-	
-	/**
-	 * If the page should be rendered with dynamic local storage support. This renders a default page that is then fetched from the server to support cordova applications
-	 *
-	 * @param localStorage
-	 */
-	public void setLocalStorage(boolean localStorage)
-	{
-		this.localStorage = localStorage;
-	}
+    /**
+     * The local storage
+     */
+    private boolean localStorage = true;
+
+    @Override
+    public @NotNull IPage<?> configure(IPage<?> page)
+    {
+        //	page.getBody()
+        //	    .addJavaScriptReference(PersistJS.getJavaScriptReference());
+        return page;
+    }
+
+    @Override
+    public boolean enabled()
+    {
+        return localStorage;
+    }
+
+
+    /**
+     * If this page should be rendered with dynamic local storage support
+     *
+     * @return
+     */
+    public boolean isLocalStorage()
+    {
+        return localStorage;
+    }
+
+    /**
+     * If the page should be rendered with dynamic local storage support. This renders a default page that is then fetched from the server to support cordova applications
+     *
+     * @param localStorage
+     */
+    public void setLocalStorage(boolean localStorage)
+    {
+        this.localStorage = localStorage;
+    }
 }
--- src/main/java/module-info.java
+++ src/main/java/module-info.java
@@ -1,23 +1,26 @@
 import com.jwebmp.plugins.security.localstorage.implementations.LocalStorageInclusionModule;
 
 module com.jwebmp.plugins.security.localstorage {
-	requires transitive com.jwebmp.core.angular;
-	
-	requires com.guicedee.guicedservlets.websockets;
-	requires jakarta.websocket;
-	requires jakarta.websocket.client;
-	
-	requires static lombok;
-	
-	provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.security.localstorage.LocalStoragePageConfigurator;
-	provides com.guicedee.guicedservlets.websockets.services.IWebSocketMessageReceiver with com.jwebmp.plugins.security.localstorage.implementations.LocalStorageWSMessageReceiver;
-	provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleInclusions with LocalStorageInclusionModule;
 
-	provides com.guicedee.guicedinjection.interfaces.IGuiceModule with com.jwebmp.plugins.security.localstorage.implementations.LocalStorageSecurityBinder;
-	
-	exports com.jwebmp.plugins.security.localstorage;
-	opens com.jwebmp.plugins.security.localstorage to com.google.guice,com.jwebmp.core;
-	
-	exports com.jwebmp.plugins.security.localstorage.implementations;
-	opens com.jwebmp.plugins.security.localstorage.implementations  to com.google.guice,com.jwebmp.core;
+    requires com.jwebmp.core.base.angular.client;
+    requires com.jwebmp.client;
+    requires com.jwebmp.core;
+
+    requires com.guicedee.guicedservlets.websockets;
+    requires jakarta.websocket;
+    requires jakarta.websocket.client;
+
+    requires static lombok;
+
+    provides com.jwebmp.core.services.IPageConfigurator with com.jwebmp.plugins.security.localstorage.LocalStoragePageConfigurator;
+    provides com.guicedee.guicedservlets.websockets.services.IWebSocketMessageReceiver with com.jwebmp.plugins.security.localstorage.implementations.LocalStorageWSMessageReceiver;
+    provides com.guicedee.guicedinjection.interfaces.IGuiceScanModuleInclusions with LocalStorageInclusionModule;
+
+    provides com.guicedee.guicedinjection.interfaces.IGuiceModule with com.jwebmp.plugins.security.localstorage.implementations.LocalStorageSecurityBinder;
+
+    exports com.jwebmp.plugins.security.localstorage;
+    opens com.jwebmp.plugins.security.localstorage to com.google.guice, com.jwebmp.core;
+
+    exports com.jwebmp.plugins.security.localstorage.implementations;
+    opens com.jwebmp.plugins.security.localstorage.implementations to com.google.guice, com.jwebmp.core;
 }
