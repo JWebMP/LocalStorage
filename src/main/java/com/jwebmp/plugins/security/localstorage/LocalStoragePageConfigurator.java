f56618c29120a9efc954a40ccec5d85d0b62dd36
GedMarc <marc.magon@gmail.com>
2024-06-03 17:59:49 +0200

    Update BlueImp Gallery and File Upload for v2 GuicedEE

--- src/main/java/com/jwebmp/plugins/security/localstorage/LocalStoragePageConfigurator.java
+++ src/main/java/com/jwebmp/plugins/security/localstorage/LocalStoragePageConfigurator.java
@@ -1,32 +1,33 @@
 package com.jwebmp.plugins.security.localstorage;
 
-import com.google.inject.*;
-import com.jwebmp.core.*;
-import com.jwebmp.core.base.angular.client.annotations.boot.*;
-import com.jwebmp.core.plugins.*;
-import com.jwebmp.core.services.*;
-import jakarta.validation.constraints.*;
+import com.google.inject.Singleton;
+import com.jwebmp.core.base.angular.client.annotations.boot.NgBootConstructorBody;
+import com.jwebmp.core.base.angular.client.annotations.boot.NgBootConstructorParameter;
+import com.jwebmp.core.plugins.PluginInformation;
+import com.jwebmp.core.plugins.PluginStatus;
+import com.jwebmp.core.services.IPage;
+import com.jwebmp.core.services.IPageConfigurator;
 
 @Singleton
 @PluginInformation(pluginName = "Local Storage ",
-        pluginUniqueName = "local-storage",
-        pluginDescription = "Provides access to read and write into the local storage of a browser.",
-        pluginVersion = "1.0",
-        pluginDependancyUniqueIDs = "jquery",
-        pluginCategories = "storage,cookies,localstorage,security",
-        pluginSubtitle = "Local Storage Service ",
-        pluginGitUrl = "https://github.com/GedMarc/JWebMP-LocalStorageSecurity",
-        pluginSourceUrl = "https://github.com/GedMarc/JWebMP-LocalStorageSecurity",
-        pluginWikiUrl = "https://github.com/GedMarc/JWebMP-LocalStorageSecurity/wiki",
-        pluginOriginalHomepage = "https://github.com/GedMarc/JWebMP-LocalStorageSecurity",
-        pluginDownloadUrl = "https://mvnrepository.com/artifact/com.jwebmp.plugins.security/jwebmp-plugins-local-storage-security",
-        pluginIconUrl = "",
-        pluginIconImageUrl = "",
-        pluginLastUpdatedDate = "2022/03/07",
-        pluginGroupId = "com.jwebmp.plugins",
-        pluginArtifactId = "localstorage",
-        pluginModuleName = "com.jwebmp.plugins.security.localstorage",
-        pluginStatus = PluginStatus.Released
+                   pluginUniqueName = "local-storage",
+                   pluginDescription = "Provides access to read and write into the local storage of a browser.",
+                   pluginVersion = "1.0",
+                   pluginDependancyUniqueIDs = "jquery",
+                   pluginCategories = "storage,cookies,localstorage,security",
+                   pluginSubtitle = "Local Storage Service ",
+                   pluginGitUrl = "https://github.com/GedMarc/JWebMP-LocalStorageSecurity",
+                   pluginSourceUrl = "https://github.com/GedMarc/JWebMP-LocalStorageSecurity",
+                   pluginWikiUrl = "https://github.com/GedMarc/JWebMP-LocalStorageSecurity/wiki",
+                   pluginOriginalHomepage = "https://github.com/GedMarc/JWebMP-LocalStorageSecurity",
+                   pluginDownloadUrl = "https://mvnrepository.com/artifact/com.jwebmp.plugins.security/jwebmp-plugins-local-storage-security",
+                   pluginIconUrl = "",
+                   pluginIconImageUrl = "",
+                   pluginLastUpdatedDate = "2022/03/07",
+                   pluginGroupId = "com.jwebmp.plugins",
+                   pluginArtifactId = "localstorage",
+                   pluginModuleName = "com.jwebmp.plugins.security.localstorage",
+                   pluginStatus = PluginStatus.Released
 )
 @NgBootConstructorParameter("private socketClientService : SocketClientService")
 @NgBootConstructorBody(value = "this.socketClientService.send('LocalStorage',{},'localStorage');", sortOrder = 5)
@@ -39,7 +40,7 @@ public class LocalStoragePageConfigurator
     private boolean localStorage = true;
 
     @Override
-    public @NotNull IPage<?> configure(IPage<?> page)
+    public IPage<?> configure(IPage<?> page)
     {
         //	page.getBody()
         //	    .addJavaScriptReference(PersistJS.getJavaScriptReference());
